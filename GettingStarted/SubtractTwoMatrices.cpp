70a4a945f4cbc041e80e9dfd0015c55c769b95e0
Anmol Rishi <anmol1rishi@gmail.com>
2018-10-25 17:54:31 +0530

    Create SubtractTwoMatrices.cpp

---
+++ GettingStarted/SubtractTwoMatrices.cpp
@@ -0,0 +1,51 @@
+//Integer pointers have been used to realise 2d matrices
+#include<iostream>
+using namespace std;
+ int main()
+{
+    int r,c,i,j,k;int *A,*B,*C;
+    char ans;
+        do{
+            cout<<"Enter the value of rows and columns of both the matrices : \n";
+            cin>>r>>c;
+            A = new int(r*c);
+            B = new int(r*c);
+            C = new int(r*c);
+            cout<<"\nEnter elements of matrix A : \n";
+            for(i=0;i<r;i++){
+                for(j=0;j<c;j++){
+                    cin>>*(A + i*c + j);
+                }
+            }
+            cout<<"\nEnter elements of matrix B : \n";
+            for(i=0;i<r;i++){
+                for(j=0;j<c;j++){
+                    cin>>*(B + i*c + j);
+                }
+            }
+            
+            //Initializing the answer matrix
+            for(i=0;i<r;i++){
+                for(j=0;j<c;j++){
+                       *(C + i*c + j) = 0;
+                }
+            }
+
+            for(i=0;i<r;i++){
+                for(j=0;j<c;j++){
+                       *(C + i*c + j) = *(A + i*c + j) - *(B + i*c + j);
+                }
+            }
+
+            cout<<"\nDifference of the matrices : \n";
+            for(i=0;i<r;i++){
+                for(j=0;j<c;j++){
+                     cout<<*(C + i*c + j) <<' ';
+                 }
+                cout<<'\n';
+            }
+             cout <<"\nAgain? (Y/N) ";
+            cin>>ans;
+        }while(ans=='y'||ans=='Y');
+    return 0;
+}
